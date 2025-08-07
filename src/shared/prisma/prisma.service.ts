1046f5e19abe05c31d3ab461fef1403c8962c769
Jon Moríñigo <mmjontxu@gmail.com>
2025-08-07 22:13:13 +0200

    Prisma

---
+++ src/shared/prisma/prisma.service.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Injectable, OnModuleInit, INestApplication } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  enableShutdownHooks(app: INestApplication) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    (this as any).$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
